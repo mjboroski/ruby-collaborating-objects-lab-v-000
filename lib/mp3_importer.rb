64440b955f466ea51103f7e59601034033b5fef2
Michael Boroski <mjboroski@gmail.com>
2018-04-24 01:41:31 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,3 +1,18 @@
 class MP3Importer
 
+  attr_accessor :path, :files
+
+  def initialize(path)
+    @path=path
+  end
+  
+  def files
+    
+  end
+
+  def import
+    directory = Dir.new(db/mp3s)
+    directory.each {|file| @files<<"#{file}"
+  end
+  
 end
